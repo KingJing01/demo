ad9e3a87fd2ac3a2583391c8028ec18c214eeaa5
liyoujing <18049808631@163.com>
2018-12-04 23:11:58 +0800

    commit sysconfig page

--- src/views/authconfig/sysconfig/page.vue
+++ src/views/authconfig/sysconfig/page.vue
@@ -9,6 +9,7 @@
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">查询</el-button>
+         <el-button type="primary" @click="dialogFormVisible  = true">新增</el-button>
       </el-form-item>
     </el-form>
     <el-table :data="tableData" style="width: 90%" border>
@@ -26,6 +27,24 @@
     <div class="block">
       <el-pagination :page-size="20" :pager-count="11" :total="1000" layout="prev, pager, next" aligen="center"/>
     </div>
+
+   <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+  <el-form :model="form">
+    <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
   </div>
 </template>
 <script>
@@ -37,7 +56,10 @@ export default {
       form: {
         sysCode: '',
         sysName: ''
-      }
+      },
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      formLabelWidth: '120px'
     }
   },
   created() {
@@ -51,12 +73,13 @@ export default {
       console.log(row)
     },
     getList() {
-      debugger
       getListData().then(response => {
-        debugger
         this.tableData = response.Data.list
       })
-    }
+    },
+    handleClose(done) {
+            done()
+      }
   }
 
 }

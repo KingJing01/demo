87e8e8501750e236a63e1c3b91b097d4ad381a0b
liyoujing <18049808631@163.com>
2018-12-04 23:14:40 +0800

    commit sysconfig page

--- src/views/authconfig/sysconfig/page.vue
+++ src/views/authconfig/sysconfig/page.vue
@@ -1,89 +1,168 @@
 <template>
   <div>
-    <el-form :inline="true" :model="form" class="demo-form-inline">
+    <el-form
+      :inline="true"
+      :model="form"
+      class="demo-form-inline"
+    >
       <el-form-item label="系统代码">
-        <el-input v-model="form.sysCode" placeholder="系统代码"/>
+        <el-input
+          v-model="form.sysCode"
+          placeholder="系统代码"
+        />
       </el-form-item>
       <el-form-item label="系统名称">
-        <el-input v-model="form.sysName" placeholder="系统名称"/>
+        <el-input
+          v-model="form.sysName"
+          placeholder="系统名称"
+        />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-         <el-button type="primary" @click="dialogFormVisible  = true">新增</el-button>
+        <el-button
+          type="primary"
+          @click="onSubmit"
+        >查询</el-button>
+        <el-button
+          type="primary"
+          @click="dialogFormVisible  = true"
+        >新增</el-button>
       </el-form-item>
     </el-form>
-    <el-table :data="tableData" style="width: 90%" border>
-      <el-table-column prop="Id" label="序号" align="center"/>
-      <el-table-column prop="SysCode" label="系统代码" align="center"/>
-      <el-table-column prop="SysName" label="系统名称" align="center"/>
-      <el-table-column prop="isValid" label="是否有效" align="center"/>
-      <el-table-column prop="address" label="操作" align="center">
+    <el-table
+      :data="tableData"
+      style="width: 90%"
+      border
+    >
+      <el-table-column
+        prop="Id"
+        label="序号"
+        align="center"
+      />
+      <el-table-column
+        prop="SysCode"
+        label="系统代码"
+        align="center"
+      />
+      <el-table-column
+        prop="SysName"
+        label="系统名称"
+        align="center"
+      />
+      <el-table-column
+        prop="isValid"
+        label="是否有效"
+        align="center"
+      />
+      <el-table-column
+        prop="address"
+        label="操作"
+        align="center"
+      >
         <template slot-scope="scope">
-          <el-button size="small" type="text" @click="handleClick(scope.row)">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <el-button
+            size="small"
+            type="text"
+            @click="handleClick(scope.row)"
+          >查看</el-button>
+          <el-button
+            type="text"
+            size="small"
+          >编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
     <div class="block">
-      <el-pagination :page-size="20" :pager-count="11" :total="1000" layout="prev, pager, next" aligen="center"/>
+      <el-pagination
+        :page-size="20"
+        :pager-count="11"
+        :total="1000"
+        layout="prev, pager, next"
+        aligen="center"
+      />
     </div>
 
-   <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-  <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" autocomplete="off"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-  <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-  </div>
-</el-dialog>
+    <el-dialog
+      title="收货地址"
+       width="40%"
+      :visible.sync="dialogFormVisible"
+    >
+      <el-form :model="form">
+        <el-form-item
+          label="活动名称"
+          :label-width="formLabelWidth"
+        >
+          <el-input
+            v-model="form.name"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="活动区域"
+          :label-width="formLabelWidth"
+        >
+          <el-select
+            v-model="form.region"
+            placeholder="请选择活动区域"
+          >
+            <el-option
+              label="区域一"
+              value="shanghai"
+            ></el-option>
+            <el-option
+              label="区域二"
+              value="beijing"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="dialogFormVisible = false"
+        >确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getListData } from '@/api/sysconfig'
+import { getListData } from "@/api/sysconfig";
 export default {
   data() {
     return {
       tableData: [],
       form: {
-        sysCode: '',
-        sysName: ''
+        sysCode: "",
+        sysName: ""
       },
       dialogTableVisible: false,
       dialogFormVisible: false,
-      formLabelWidth: '120px'
-    }
+      formLabelWidth: "120px"
+    };
   },
   created() {
-    this.getList()
+    this.getList();
   },
   methods: {
     onSubmit() {
-      console.log('submit!')
+      console.log("submit!");
     },
     handleClick(row) {
-      console.log(row)
+      console.log(row);
     },
     getList() {
       getListData().then(response => {
-        this.tableData = response.Data.list
-      })
+        this.tableData = response.Data.list;
+      });
     },
     handleClose(done) {
-            done()
-      }
+      done();
+    }
   }
-
-}
-
+};
 </script>
 <style>
 .el-row {
@@ -107,5 +186,4 @@ export default {
   padding: 10px 0;
   background-color: #f9fafc;
 }
-
 </style>

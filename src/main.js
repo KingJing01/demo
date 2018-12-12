1e808fa5799fa0cc31f0c62c770ad60f613ee6ed
KingJing01 <18049808631@163.com>
2018-12-12 17:57:09 +0800

    系统信息分页完善

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import enLocale from 'element-ui/lib/locale/lang/en'
 
 import '@/styles/index.scss' // global css
 
@@ -15,7 +16,7 @@ import store from './store'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale, enLocale })
 
 Vue.config.productionTip = false
 

63304b5f7d401f3a5f540c70184a4bea1f6f0d7f
KingJing01 <18049808631@163.com>
2018-12-20 18:24:53 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -29,7 +29,7 @@ const user = {
     Login({ commit }, userInfo) {
       const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
-        login(username, userInfo.password, userInfo.sysCode).then(response => {
+        login(username, userInfo.password).then(response => {
           setToken(response.Token)
           commit('SET_TOKEN', response.Token)
           resolve()
